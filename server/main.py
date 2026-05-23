70854cc4126ccc0311af66ef283e1cc9fb02c41e
samuelikohn <samuelikohn@gmail.com>
2026-05-22 23:24:53 -0500

    removed hello world route

--- server/main.py
+++ server/main.py
@@ -16,10 +16,5 @@ app.include_router(query_router, prefix=API_PREFIX)
 app.include_router(document_router, prefix=API_PREFIX)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
